d98fd02014225bb8b7cde26bd3b7974706e2021e
theusaf <theusafyt@gmail.com>
2023-12-26 15:26:34 -0800

    feat(user): initialize user page

---
+++ src/app/user/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function UserPage({
+  params,
+}: {
+  params: Record<string, string>;
+}) {
+  return <main>
+    <h1 className="text-2xl mb-2">{params.id}&rsquo;s profile</h1>
+  </main>
+}
