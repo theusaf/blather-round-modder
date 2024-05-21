c2a721e7b33365f783818cbce6b7b9ce9aef27b2
theusaf <theusafyt@gmail.com>
2024-05-21 10:55:04 -0700

    feat(final): add navigation bar

---
+++ src/app/(registration)/layout.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react";
+
+export default function RegistrationLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return <div>
+    <h1>Test</h1>
+    {children}
+  </div>
+}
