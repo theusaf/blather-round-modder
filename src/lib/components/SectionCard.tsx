b66820e74569cca541548e59bf5698f8cb6b3002
theusaf <theusafyt@gmail.com>
2024-05-25 14:36:13 -0700

    fix(final): add lib directory to tailwind config

--- src/lib/components/SectionCard.tsx
+++ src/lib/components/SectionCard.tsx
@@ -6,7 +6,7 @@ export default function SectionCard(
   return (
     <div
       {...props}
-      className={`border-slate-300 border-2 rounded-md shadow-sm shadow-black p-2 ${props.className ?? ""}`}
+      className={`border-slate-400 border-2 rounded-md shadow-sm shadow-black p-2 ${props.className ?? ""}`}
     />
   );
 }
