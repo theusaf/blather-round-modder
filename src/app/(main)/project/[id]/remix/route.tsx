a3b68797ce62c275a2aac5db99078488c33a8ea5
theusaf <theusafyt@gmail.com>
2024-06-02 17:18:02 -0700

    feat: implement remix route

--- src/app/(main)/project/[id]/remix/route.tsx
+++ src/app/(main)/project/[id]/remix/route.tsx
@@ -1,3 +1,41 @@
-export function GET() {
-  // TODO: Fetch project data from the server
+import "server-only";
+import { NextRequest, NextResponse } from "next/server";
+import { getUserSession } from "@/lib/util/auth";
+import { redirect } from "next/navigation";
+import Project from "@/lib/database/models/project";
+
+export async function GET(
+  _: NextRequest,
+  { params }: { params: { id: string } },
+) {
+  const id = params.id;
+  const user = await getUserSession();
+  if (!user) return redirect("/login");
+  const baseProject = await Project.findById(id);
+  if (!baseProject) {
+    return NextResponse.json(
+      {
+        error: "Project not found",
+      },
+      { status: 404 },
+    );
+  }
+  if (baseProject.ownerId !== user.sub && !baseProject.public) {
+    return NextResponse.json(
+      {
+        error: "You do not have permission to remix this project",
+      },
+      { status: 403 },
+    );
+  }
+  const project = new Project({
+    name: `Copy of ${baseProject.name}`,
+    ownerId: user.sub,
+    public: false,
+    prompts: baseProject.prompts,
+    sentenceStructures: baseProject.sentenceStructures,
+    wordLists: baseProject.wordLists,
+  });
+  await project.save();
+  return redirect(`/project/${project.id}/edit`);
 }
