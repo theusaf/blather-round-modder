b26dc396c1fb484716e71676dd7e047600a279ac
theusaf <theusafyt@gmail.com>
2024-06-05 10:48:52 -0700

    fix(project/edit): fix sentence structure overflow

--- src/lib/components/ListInputField.tsx
+++ src/lib/components/ListInputField.tsx
@@ -57,9 +57,9 @@ export function ListInputField(
   return (
     <div
       {...divProps}
-      className={`border-2 border-slate-400 rounded-md h-14 cursor-text flex items-center gap-2 ${props.className ?? ""}`}
+      className={`border-2 border-slate-400 rounded-md h-14 cursor-text grid overflow-x-auto grid-cols-2 items-center gap-2 ${props.className ?? ""}`}
     >
-      <div className="flex-1 items-center h-full flex border-slate-400 border-r-2 rounded-md p-2">
+      <div className="items-center h-full flex border-slate-400 border-r-2 rounded-md p-2">
         <input
           className="w-full"
           value={value}
@@ -67,7 +67,7 @@ export function ListInputField(
           placeholder="Type here..."
         />
       </div>
-      <div className="h-full flex-1 overflow-x-auto whitespace-nowrap rounded-md items-center flex border-l-2 border-slate-400 p-2">
+      <div className="h-full whitespace-nowrap rounded-md items-center flex border-l-2 border-slate-400 p-2">
         {displayItems}
       </div>
     </div>
