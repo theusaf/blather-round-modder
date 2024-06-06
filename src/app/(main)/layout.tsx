2ecdecee2d58de096ef9ab358be23890d6f491c0
theusaf <theusafyt@gmail.com>
2024-06-06 14:16:06 -0700

    feat: add basic home content

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { NavBar } from "./_components/NavBar";
 
 export default async function RootLayout({
@@ -10,7 +11,18 @@ export default async function RootLayout({
       <NavBar />
       <div className="flex-1">{children}</div>
       <footer className="p-8 bg-lime-700 text-white">
-        &copy; 2024 Daniel Lau
+        <div className="flex justify-between md:flex-row flex-col">
+          <span className="flex items-center">&copy; 2024 Daniel Lau</span>
+          <div className="md:w-1/2">
+            <span>
+              This website is not affiliated, associated with, authorized,
+              endorsed by, or in any way officially connected with{" "}
+              <Link href="https://www.jackboxgames.com/">Jackbox Games</Link> or
+              any of its subsidiaries or affiliates.
+            </span>{" "}
+            <span>Blather &apos;Round is a trademak of Jackbox Games.</span>
+          </div>
+        </div>
       </footer>
     </div>
   );
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,13 +1,41 @@
+import Link from "next/link";
 import AnimatedHomeSection from "./_components/AnimatedHomeSection";
 
 export default function Home() {
   return (
     <main className="p-2">
-      <h1 className="text-2xl font-bold">Editor for Blather 'Round</h1>
+      <h1 className="text-2xl font-bold">Editor for Blather &apos;Round</h1>
+      <p className="text-lg">
+        Editor for Blather &apos;Round is a website which allows you to easily
+        create and customize your Blather &apos;Round experience.
+      </p>
+      <article className="text-lg">
+        <h3 className="text-xl font-bold">How to use</h3>
+        <p>
+          To get started,{" "}
+          <Link href="/project/create" className="text-blue-400">
+            create a project
+          </Link>
+          ! You may need to create an account or log in to get started.
+        </p>
+        <p>
+          In addition, you can browse existing{" "}
+          <Link href="/projects" className="text-blue-400">
+            public projects
+          </Link>{" "}
+          to try out or start a new projects from.
+        </p>
+        <p className="mt-2">
+          In the project editor, you can quickly switch and search through word
+          lists and prompts. The editor also includes helpful tooltips to
+          explain what each field does. Make sure to save your project to avoid
+          losing changes by pressing the save button.
+        </p>
+      </article>
+      <br />
       <div>
         <AnimatedHomeSection />
       </div>
-      <article>more content</article>
     </main>
   );
 }
