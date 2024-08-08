70326254b086d62bc22b1b5841be0a1e24928dfe
theusaf <theusafyt@gmail.com>
2024-08-07 17:02:46 -0700

    feat(sections/wordList): Add option to add word list button
    Enables faster creation of response word lists, albeit just clones the base response sentence
    Resolves #1

--- src/app/project/[id]/edit/_components/sections/wordLists.tsx
+++ src/app/project/[id]/edit/_components/sections/wordLists.tsx
@@ -8,6 +8,8 @@ import { ListEditModal } from "../ListEditModal";
 import { produce } from "immer";
 import { WordListing } from "../WordListing";
 import { filterWordList } from "../../_util/filterWordList";
+import { Tooltip } from "@mui/material";
+import OutsideClickDetector from "@/lib/components/OutsideClickDetector";
 
 export default function WordListSection() {
 	const wordLists = useProjectStore((state) => state.wordLists);
@@ -35,25 +37,7 @@ export default function WordListSection() {
 							onChange={(e) => setSearch(e.target.value)}
 						/>
 					</div>
-					<div>
-						<button
-							type="button"
-							className="flex items-center h-full"
-							onClick={() => {
-								setListModal({
-									amount: "",
-									id: "000",
-									maxChoices: "",
-									name: "",
-									optional: false,
-									placeholder: "",
-									words: [],
-								});
-							}}
-						>
-							<FontAwesomeIcon className="w-8 h-8" icon={faPlusCircle} />
-						</button>
-					</div>
+					<AddWordListButton onSelect={setListModal} />
 				</div>
 				<hr className="my-2" />
 				<WordListing setModal={setListModal} wordLists={filteredWordLists} />
@@ -87,5 +71,101 @@ export default function WordListSection() {
 				open={listModal !== null}
 			/>
 		</>
+	);
+}
+
+function AddWordListButton({
+	onSelect,
+}: {
+	onSelect: (value: WordListType) => void;
+}) {
+	const [choiceOpen, setChoiceOpen] = useState(false);
+
+	return (
+		<div>
+			<Tooltip
+				open={choiceOpen}
+				placement="left"
+				title={
+					<OutsideClickDetector onClickOutside={() => setChoiceOpen(false)}>
+						<div className="flex flex-col gap-2 my-2">
+							<button
+								onClick={() => {
+									setChoiceOpen(false);
+									onSelect({
+										amount: "",
+										id: "000",
+										maxChoices: "",
+										name: "",
+										optional: false,
+										placeholder: "",
+										words: [],
+									});
+								}}
+								type="button"
+								className="text-left font-sans p-2 bg-slate-300 border-slate-600 border-2 text-black cursor-pointer"
+							>
+								<h4 className="font-bold text-base">Blank List</h4>
+								<p className="text-sm">Regular collection of words</p>
+							</button>
+							<button
+								onClick={() => {
+									const responsePhrases =
+										useProjectStore
+											.getState()
+											.wordLists.find(
+												(list) => list.name === "response-sentence",
+											)
+											?.words.map((word) => {
+												return {
+													...word,
+													alwaysChoose: true,
+												};
+											}) ??
+										[
+											"It's a lot like",
+											"It's kinda similar to",
+											"It's nothing like",
+											"It has the same vibe as",
+											"It's a different genre than",
+											"It's not the same form as",
+										].map((word) => {
+											return {
+												alwaysChoose: true,
+												word,
+											};
+										});
+									setChoiceOpen(false);
+									onSelect({
+										amount: "",
+										id: "000",
+										maxChoices: "1",
+										name: "response-sentence-EDITME",
+										optional: false,
+										placeholder: "It's a lot like",
+										words: responsePhrases,
+									});
+								}}
+								type="button"
+								className="text-left p-2 bg-slate-300 border-slate-600 border-2 text-black cursor-pointer"
+							>
+								<h4 className="font-bold text-base">Response List</h4>
+								<p className="text-sm">
+									Collection of phrases for response sentences
+								</p>
+							</button>
+						</div>
+					</OutsideClickDetector>
+				}
+			>
+				<button
+					type="button"
+					className="flex items-center h-full"
+					onClick={() => setChoiceOpen(true)}
+				>
+					<FontAwesomeIcon className="w-8 h-8" icon={faPlusCircle} />
+				</button>
+			</Tooltip>
+		</div>
 	);
 }
