779b2d7c4b8ab56ab70d03a80c61bb9025b0344f
Daniel Lau <theusafyt@gmail.com>
2025-03-18 21:55:41 -0700

    feat(TailoredWordEditSection): allow enter to submit a new tailored word

--- src/app/project/[id]/edit/_components/PromptEditModal.tsx
+++ src/app/project/[id]/edit/_components/PromptEditModal.tsx
@@ -194,48 +194,60 @@ export function PromptEditModal({
 				/>
 				<div className="grid grid-cols-3 gap-2 overflow-hidden">
 					<div className="border-r-2 border-slate-600 mt-2">
-						<h4>Suggestions</h4>
+						<h4 className="font-semibold text-lg">Suggestions</h4>
 					</div>
-					<div className="grid md:grid-cols-3 gap-2 mt-2 col-span-2">
-						{promptData.tailoredWords.map((word, index) => (
-							<SectionCard key={index} className="overflow-hidden">
-								<div className="flex gap-2 items-center justify-between">
-									<div className="flex gap-2 items-center min-w-0">
-										<WordListTile
-											onClose={(list) => {
-												if (!list) return;
-												setPromptData(
-													produce(promptData, (draft) => {
-														draft.tailoredWords[index].list = `<${list}>`;
-													}),
-												);
-											}}
-											list={word.list.slice(1, -1)}
-										/>
-										<span className="truncate" title={word.word}>
-											{word.word}
-										</span>
-									</div>
-									<button
-										type="button"
-										className="flex items-center h-min"
-										onClick={() => {
-											setPromptData(
-												produce(promptData, (draft) => {
-													draft.tailoredWords.splice(index, 1);
-												}),
-											);
-										}}
-									>
-										<FontAwesomeIcon className="w-6 h-6" icon={faTrash} />
-									</button>
-								</div>
-							</SectionCard>
-						))}
-					</div>
+					<TailoredWordTileList
+						promptData={promptData}
+						setPromptData={setPromptData}
+					/>
 				</div>
 			</div>
 		</CenteredModal>
+	);
+}
+
+function TailoredWordTileList({
+	promptData,
+	setPromptData,
+}: { promptData: PromptType; setPromptData: (data: PromptType) => void }) {
+	return (
+		<div className="grid md:grid-cols-3 gap-2 mt-2 col-span-2">
+			{promptData.tailoredWords.map((word, index) => (
+				<SectionCard key={index} className="overflow-hidden">
+					<div className="flex gap-2 items-center justify-between">
+						<div className="flex gap-2 items-center min-w-0">
+							<WordListTile
+								onClose={(list) => {
+									if (!list) return;
+									setPromptData(
+										produce(promptData, (draft) => {
+											draft.tailoredWords[index].list = `<${list}>`;
+										}),
+									);
+								}}
+								list={word.list.slice(1, -1)}
+							/>
+							<span className="truncate" title={word.word}>
+								{word.word}
+							</span>
+						</div>
+						<button
+							type="button"
+							className="flex items-center h-min"
+							onClick={() => {
+								setPromptData(
+									produce(promptData, (draft) => {
+										draft.tailoredWords.splice(index, 1);
+									}),
+								);
+							}}
+						>
+							<FontAwesomeIcon className="w-6 h-6" icon={faTrash} />
+						</button>
+					</div>
+				</SectionCard>
+			))}
+		</div>
 	);
 }
 
--- src/app/project/[id]/edit/_components/TailoredWordEditSection.tsx
+++ src/app/project/[id]/edit/_components/TailoredWordEditSection.tsx
@@ -16,53 +16,56 @@ export function TailoredWordEditSection({
 	return (
 		<div>
 			<h4 className="text-lg font-semibold">Tailored Word</h4>
-			<div className="flex gap-2">
-				<LabeledInput
-					label="Add Tailored Word"
-					name="modal-list-tailored-word"
-					inputId="modal-list-tailored-word"
-					placeholder="Enter tailored word or <list>"
-					tooltip="May make some words more likely or add a custom related word to a list. It is recommended to have at least 10 per prompt."
-					value={word}
-					className="w-0"
-					onValueChange={(value) => {
-						setWord(value);
-					}}
-				/>
-				<LabeledInput
-					label="List to Add to"
-					name="modal-list-tailored-list"
-					inputId="modal-list-tailored-list"
-					placeholder="Enter list"
-					value={list}
-					className="w-0"
-					onValueChange={(value) => {
-						setList(value);
-					}}
-				/>
-				<div className="flex items-end">
-					<WordListTile
-						list="Search!"
-						useValue={false}
-						onClose={(selection) => {
-							if (!selection) return;
-							setList(selection);
+			<form
+				onSubmit={(evt) => {
+					evt.preventDefault();
+					if (word.trim() === "" || list.trim() === "") return;
+					onSubmit(word, list);
+					setWord("");
+					setList("");
+				}}
+			>
+				<div className="flex gap-2">
+					<LabeledInput
+						label="Add Tailored Word"
+						name="modal-list-tailored-word"
+						inputId="modal-list-tailored-word"
+						placeholder="Enter tailored word or <list>"
+						tooltip="May make some words more likely or add a custom related word to a list. It is recommended to have at least 10 per prompt."
+						value={word}
+						className="w-0"
+						type="search"
+						onValueChange={(value) => {
+							setWord(value);
 						}}
 					/>
+					<LabeledInput
+						label="List to Add to"
+						name="modal-list-tailored-list"
+						inputId="modal-list-tailored-list"
+						placeholder="Enter list"
+						value={list}
+						className="w-0"
+						type="search"
+						onValueChange={(value) => {
+							setList(value);
+						}}
+					/>
+					<div className="flex items-end">
+						<WordListTile
+							list="Search!"
+							useValue={false}
+							onClose={(selection) => {
+								if (!selection) return;
+								setList(selection);
+							}}
+						/>
+					</div>
+					<button type="submit" className="flex items-end mb-2 cursor-pointer">
+						<FontAwesomeIcon className="w-12 h-12" icon={faPlusCircle} />
+					</button>
 				</div>
-				<button
-					type="button"
-					className="flex items-end mb-2 cursor-pointer"
-					onClick={() => {
-						if (word.trim() === "" || list.trim() === "") return;
-						onSubmit(word, list);
-						setWord("");
-						setList("");
-					}}
-				>
-					<FontAwesomeIcon className="w-12 h-12" icon={faPlusCircle} />
-				</button>
-			</div>
+			</form>
 		</div>
 	);
 }
