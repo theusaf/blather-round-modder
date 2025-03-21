1184e280fcc5abed7d84d75225abf5728fbfa46a
Daniel Lau <theusafyt@gmail.com>
2025-03-20 18:10:47 -0700

    fix(list): properly add tailored words and strings

--- src/lib/util/list.tsx
+++ src/lib/util/list.tsx
@@ -22,7 +22,7 @@ export function getListMaps({
 	}
 	// add words from tailored words
 	for (const word of tailoredWords) {
-		const list = listMap[word.list];
+		const list = listMap[word.list.slice(1, -1)];
 		if (list) {
 			list.words.push({ word: word.word, alwaysChoose: false });
 		}
@@ -91,27 +91,31 @@ export function getListMaps({
 	);
 
 	// TODO: this doesn't work. might need to be recursive.
-	for (const key in listMap) {
-		const strings = [];
-		for (const word of listMap[key].words) {
-			if (word.word.startsWith("<") && word.word.endsWith(">")) {
-				const list = word.word.slice(1, -1);
-				const listRelatives = topLevelListMap[list];
-				if (!listRelatives) continue;
-				for (const relative of listRelatives) {
-					const relativeWords = listMap[relative]?.words;
-					if (!relativeWords) continue;
-					strings.push(
-						...relativeWords.filter(
-							(a) => !(a.word.startsWith("<") && a.word.endsWith(">")),
-						),
-					);
-				}
+	const recursiveAddWord = (
+		listName: string,
+		discoveredLists = new Set<string>(),
+	) => {
+		const strings: WordListType["words"] = [];
+		const list = listMap[listName];
+		if (!list) return [];
+		const lists = [];
+		for (const word of list.words) {
+			if (/^<[^>]+>$/.test(word.word)) {
+				lists.push(word.word.slice(1, -1));
 			} else {
 				strings.push(word);
 			}
 		}
-		listWordMap[key] = strings;
+		if (lists.length === 0) return strings;
+		for (const sublist of lists) {
+			if (discoveredLists.has(sublist)) continue; // prevent infinite recursion
+			discoveredLists.add(sublist);
+			strings.push(...recursiveAddWord(sublist, discoveredLists));
+		}
+		return strings;
+	};
+	for (const key in listMap) {
+		listWordMap[key] = recursiveAddWord(key);
 	}
 	return {
 		topLevelListKeys,
