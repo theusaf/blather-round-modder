4752fb7d96c4a7d8ed17259fdd3fbb36b55bb23d
Daniel Lau <daniel.lau@qmfremont.com>
2025-04-17 15:45:02 -0700

    feat: add version number to footer

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,3 +1,4 @@
+import "server-only";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
@@ -14,16 +15,21 @@ export default async function RootLayout({
 			<div className="flex-1">{children}</div>
 			<footer className="p-8 bg-lime-700 text-white">
 				<div className="flex justify-between md:flex-row flex-col">
-					<div className="flex flex-wrap flex-col gap-4">
-						<div>&copy; 2024 Daniel Lau</div>
-						<div>
-							<Link
-								className="flex gap-1 items-center"
-								href="https://github.com/theusaf/blather-round-modder"
-							>
-								<FontAwesomeIcon icon={faGithub} className="h-4" />
-								<span>GitHub</span>
-							</Link>
+					<div className="grid grid-flow-col gap-8">
+						<div className="flex flex-wrap flex-col gap-4">
+							<div>&copy; 2024 Daniel Lau</div>
+							<div>
+								<Link
+									className="flex gap-1 items-center"
+									href="https://github.com/theusaf/blather-round-modder"
+								>
+									<FontAwesomeIcon icon={faGithub} className="h-4" />
+									<span>GitHub</span>
+								</Link>
+							</div>
+						</div>
+						<div className="flex flex-wrap flex-col gap-4">
+							<div>Version {process.env.npm_package_version}</div>
 						</div>
 					</div>
 					<div className="md:w-1/2">
